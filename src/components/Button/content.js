6f13b9a26e46751652c06b96ee6d3874f1d75dbd
Nasidul Islam <nasidulislam@gmail.com>
2020-04-23 16:09:56 -0400

    init-app: complete application setup.

---
+++ src/components/Button/content.js
@@ -0,0 +1,3 @@
+export default {
+    containerClass: 'button-container'
+}
---
+++ src/core/js/classList.js
@@ -0,0 +1,6 @@
+// this helper file generate a class list for elements
+const classList = (base, additional) => {
+    return base + ' ' + (additional || '');
+  };
+  
+  export default classList;
