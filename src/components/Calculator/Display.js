b5e37ac8869bac9620fedf1ac26364a6c6126c6e
Nasidul Islam <nasidulislam@gmail.com>
2020-04-28 13:25:59 -0400

    display: minor refactor.

--- src/components/Calculator/Display.js
+++ src/components/Calculator/Display.js
@@ -5,25 +5,13 @@ import content from './content';
 import classList from '../../core/js/classList';
 
 export default function Display(props) {
-    function handleSecondaryDisplay() {
-        let secondaryDisplay = props.firstInput + ' ' + props.operator + ' ' + props.secondInput;
-
-        return secondaryDisplay;
-    }
-
-    function handlePrimaryDisplay() {
-        let primaryDisplay = props.result;
-
-        return primaryDisplay;
-    }
-
     return(
         <div className={classList(content.display.containerClass, props.classList)}>
             <div className={content.display.secondaryDisplayContainerClass}>
-                {handleSecondaryDisplay()}
+                {props.firstInput + ' ' + props.operator + ' ' + props.secondInput}
             </div>
             <div className={content.display.primaryDisplayContainerClass}>
-                {handlePrimaryDisplay()}
+                {props.result}
             </div>
         </div>
     )
